9818389b8232f1c4bba604a89af1e468a55b6704
Kunto Mustiko <kuntomustiko@gmail.com>
2020-01-28 14:51:35 +0700

    Hari pertama Javascript Fundamental

---
+++ Latihan-Hari-1/index.js
@@ -0,0 +1,30 @@
+var apel = prompt("masukkan jumlah apel");
+// var jeruk = prompt("masukkan jumlah jeruk");
+// var mangga = prompt("masukkan jumlah mangga");
+var x;
+x.format(10000);
+console.log(x);
+// var result = apel + 10000.toLocaleString();
+// console.log(result)
+
+// var hargaApel = 10000;
+// var hargaJeruk = 15000;
+// var hargaMangga = 20000;
+
+// // console.log(hargaApel.toLocaleString());
+
+// var hasilApel = apel * hargaApel;
+// var hasilJeruk = jeruk * hargaJeruk;
+// var hasilMangga = mangga * hargaMangga;
+// var totalSeluruhHarga = hasilApel + hasilJeruk + hasilMangga;
+
+// alert("1. " + "Anda Membeli " + apel + " APEL " + " dengan harga " + hargaApel.toLocaleString() + " dengan total harga " + hasilApel + "\n" +
+//     "2. " + "Anda Membeli " + jeruk + " JERUK " + " dengan harga " + hargaJeruk.toLocaleString() + " dengan total harga " + hasilJeruk + "\n" +
+//     "3. " + "Anda Membeli " + mangga + " MANGGA " + " dengan harga " + hargaMangga.toLocaleString() + " dengan total harga " + hasilMangga + "\n" +
+//     "Total Harga seluruhnya " + totalSeluruhHarga);
+
+
+// console.log("1. " + "Anda Membeli " + apel + " APEL " + " dengan harga " + hargaApel + " dengan total harga " + hasilApel + "\n" +
+//     "2. " + "Anda Membeli " + jeruk + " JERUK " + " dengan harga " + hargaJeruk + " dengan total harga " + hasilJeruk + "\n" +
+//     "3. " + "Anda Membeli " + mangga + " MANGGA " + " dengan harga " + hargaMangga + " dengan total harga " + hasilMangga + "\n" +
+//     "Total Harga seluruhnya " + totalSeluruhHarga);
